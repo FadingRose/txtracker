40575a97bccc6694cf7663a406e52b05454fafb8
KobayakawaAMi <kobayakawaami@gmail.com>
2024-04-02 16:43:58 +0800

    init: create project.

---
+++ build.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+chmod +x build.zsh
+
+set -e
+
+BUILD_DIR="./build/bin"
+EXECUTABLE_NAME="txtracker"
+
+echo "Cleaning up existing build directory..."
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+
+echo "Running automated tests..."
+go test ./...
+
+echo "Building the project..."
+go build -o "${BUILD_DIR}/${EXECUTABLE_NAME}" ./cmd/txtracker
+
+echo "Build successful! Executable is located at ${BUILD_DIR}/${EXECUTABLE_NAME}"
