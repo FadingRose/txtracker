2d9ea6154b0dc3848aea486194caa3b0d73d49b9
kobayakawaami <kobayakawaami@gmail.com>
2024-04-04 21:32:28 +0800

    modify: platform check

--- pkg/compiler/compiler.go
+++ pkg/compiler/compiler.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"txtracker/pkg/logger"
+	"runtime"
 )
 
 // Compiler is an interface that defines the methods that a compiler must implement.
@@ -23,8 +25,22 @@ func NewSolidityCompiler() Compiler {
 func (s *SolidityCompiler) SolidityToAST_JSON(SolidityPath string) error {
 	fmt.Println("SolidityToAST_JSON called with path:", SolidityPath)
 
+	
 	// source virtual environment at ../../venv/bin/activate
-	cmdString := "source ../../venv/bin/activate && solc --ast-compact-json " + SolidityPath
+	var cmdString string
+	
+	if runtime.GOOS == "darwin"{
+		fmt.Println("macOS/Linux detected")
+		cmdString = "solc --ast-compact-json " + SolidityPath
+	} else if runtime.GOOS == "windows" {
+		fmt.Println("windows detected")
+		cmdString = "source ../../venv/bin/activate && solc --ast-compact-json " + SolidityPath
+	} else {
+		fmt.Println("OS not detected")
+		logger.Fatal.Println("OS not detected. ")
+		panic("OS not detected.")
+	}
+	
 	cmd := exec.Command("zsh", "-c", cmdString)
 
 	// remove .sol and add .ast.json
