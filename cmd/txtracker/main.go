374350cdd18f89869158dac6c131cd1191f65692
kobayakawaami <kobayakawaami@gmail.com>
2024-04-08 02:25:07 +0800

    modify: main.go can receive args for specific file now.

--- cmd/txtracker/main.go
+++ cmd/txtracker/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"txtracker/pkg/compiler"
 	"txtracker/pkg/filehandler"
+	"txtracker/pkg/logger"
 	"txtracker/pkg/parser"
 	"txtracker/pkg/printer"
 )
 
 func main() {
-	filehandler, err := filehandler.NewFileHandler("../../dataset/contracts")
+
+	var SPECIFIC_CONTRACT string
+	if len(os.Args) >= 2 {
+		SPECIFIC_CONTRACT = os.Args[1]
+	} else {
+		SPECIFIC_CONTRACT = ""
+
+	}
+
+	filehandler, err := filehandler.NewFileHandler("../../dataset/contracts", SPECIFIC_CONTRACT)
 	if err != nil {
 		panic(err)
 	}
@@ -22,6 +33,7 @@ func main() {
 		fmt.Println("Processing:", path)
 		err := compiler.SolidityToAST_JSON(path)
 		if err != nil {
+			logger.Fatal.Println("Error compiling:", err)
 			panic(err)
 		}
 		astFilePath := path + ".ast.json"
